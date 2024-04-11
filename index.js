bac2e8bfa0f609b57f7c9e7389c077e8c9690e4f
faisalpinitod <faisalpinitod@gmail.com>
2024-04-11 22:45:43 +0530

    completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 
 const connection = mongoose.connect(
   "mongodb+srv://Faisalpinitod:faisal@cluster0.y2f7t.mongodb.net/crashcourse-1?retryWrites=true&w=majority");
@@ -14,69 +15,87 @@ const User = mongoose.model("User", userSchema);
 const app = express();
 app.use(express.json());
 
-// Routes
-// Create a user
-app.post("/users", async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 });
 
-// Get all users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Get a user by ID
-app.get("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send();
+// Authentication middleware
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
     }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 
-// Update a user by ID
-app.patch("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
+    jwt.verify(token, 'MASAI', (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+        req.user = user;
+        next();
     });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+};
+
+
+
+
+// CRUD routes
+app.post('/api/users', (req, res) => {
+    const newUser = new User(req.body);
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).json(err);
+        }
+        res.status(201).json(user);
+    });
 });
 
-// Delete a user by ID
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+app.get('/api/users', authenticateJWT, (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    User.find()
+        .skip(skip)
+        .limit(limit)
+        .exec((err, users) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            res.json(users);
+        });
 });
+
+app.get('/api/users/:userId', authenticateJWT, (req, res) => {
+    User.findById(req.params.userId, (err, user) => {
+        if (err) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    });
+});
+
+app.put('/api/users/:userId', authenticateJWT, (req, res) => {
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true }, (err, user) => {
+        if (err) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    });
+});
+
+app.delete('/api/users/:userId', authenticateJWT, (req, res) => {
+    User.findByIdAndDelete(req.params.userId, (err, user) => {
+        if (err) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(204).end();
+    });
+});
+
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
